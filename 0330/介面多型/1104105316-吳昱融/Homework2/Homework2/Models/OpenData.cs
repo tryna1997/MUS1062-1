187dff6dfed71eba0a4f9adaf24add0d48e800a7
ai4rmp <1104105316@gm.kuas.edu.tw>
2018-04-19 23:55:01 +0800

    3/30作業

---
+++ 0330/介面多型/1104105316-吳昱融/Homework2/Homework2/Models/OpenData.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Homework2.Models
+{
+    class OpenData
+    {
+        public string 年月別 { get; set; }
+        public string 最近月份收益率 { get; set; }
+        public string 公告日期 { get; set; }
+    }
+}
